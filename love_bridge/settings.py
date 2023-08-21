f43f87798833089901b23dcb305bc4e446f5ea94
genieu99 <74898231+genieu99@users.noreply.github.com>
2023-08-22 00:55:29 +0900

    [Chore] ALLOWED_HOSTS 변경

--- love_bridge/settings.py
+++ love_bridge/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
